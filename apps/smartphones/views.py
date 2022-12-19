c3d4baef8a892a40fc1436076be1fd86d34b798c
Sadyk <begaliev.sadyk17@gmail.com>
2022-12-19 23:09:20 +0600

    23:09

--- apps/smartphones/views.py
+++ apps/smartphones/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Brand, Smartphone
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from .serializers import (BrandSerializer, SmartphoneListSerializer, SmartphoneSerializer)
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.generics import (ListAPIView, ListCreateAPIView, CreateAPIView, UpdateAPIView,
                                      RetrieveAPIView, RetrieveUpdateDestroyAPIView)
 
@@ -8,23 +11,39 @@ from rest_framework.generics import (ListAPIView, ListCreateAPIView, CreateAPIVi
 class SmartphoneListAPIView(ListAPIView):
     queryset = Smartphone.objects.all()
     serializer_class = SmartphoneListSerializer
+    permission_classes = [AllowAny]
     
     
 class SmartphoneAPIView(RetrieveAPIView):
     queryset = Smartphone.objects.all()
     serializer_class = SmartphoneSerializer
+    permission_classes = [AllowAny]
     lookup_field = 'slug'
 
 
 class SmartphoneCreateAPIView(CreateAPIView):
     queryset = Smartphone.objects.all()
     serializer_class = SmartphoneSerializer
+    permission_classes = [IsAdminUser]
+
+    # def get_permissions(self):
+    #     if self.action in ['create', 'destroy', 'update', 'partial_update']:
+    #         self.permission_classes = [IsAdminUser]
+    #     if self.action in ['list', 'retrieve']:
+    #         self.permission_classes = [AllowAny]
 
 
 class SmartphoneUpdateAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Smartphone.objects.all()
     serializer_class = SmartphoneSerializer
+    permission_classes = [IsAdminUser]
     lookup_field = 'slug'
+
+    # def get_permissions(self):
+    #     if self.action in ['create', 'destroy', 'update', 'partial_update']:
+    #         self.permission_classes = [IsAdminUser]
+    #     if self.action in ['list', 'retrieve']:
+    #         self.permission_classes = [AllowAny]
 
 
 class BrandListAPIView(ListAPIView):
